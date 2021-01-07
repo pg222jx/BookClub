535175cc3205918449e9544b938f27355bd43160
Filippa Jakobsson <fj222nq@student.lnu.se>
2021-01-07 11:24:57 +0100

    adding menu

--- view/Menu.js
+++ view/Menu.js
@@ -1,9 +1,41 @@
+const inquirer = require('inquirer') 
 
 class Menu {
 //   constructor () {
 //   }
   sayhello() {
-      console.log('Hello World')
+    console.log('Welcome to Book Club!')
+    console.log('_________________________________\n')
+    const start = [
+      {
+        type: 'list',
+        name: 'menu',
+        message: 'Menu',
+        choices: ['Member Information', 'Member Reviews', 'List of Members', 'Book Information', 'Quit'],
+        filter: function (val) {
+          return val.toLowerCase()
+        }
+      }
+    ]
+  
+    inquirer.prompt(start).then(answers => {
+      if (answers.menu === 'member information') {
+        console.log('MEMBER INFO')
+        // Get member info
+      } else if (answers.menu === 'member reviews') {
+        console.log('MEMBER REVIEWS')
+        // Get member reviews
+      } else if (answers.menu === 'list of members') {
+        console.log('LIST OF MEMBERS')
+        // Get member list
+      } else if (answers.menu === 'book information') {
+        console.log('BOOK INFO')
+        // Get book info
+      } else if (answers.menu === 'quit') {
+        console.log('\nWelcome back!\n')
+        process.exit(0)
+      }
+    })
   }
 }
 
