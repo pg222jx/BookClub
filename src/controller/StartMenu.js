85bd8a525ecbb871467cc1b0fb8ced98904d6778
Pernilla Göth <pg222jx@student.lnu.se>
2021-01-11 11:32:22 +0100

    Refactoring listMenu

--- src/controller/StartMenu.js
+++ src/controller/StartMenu.js
@@ -55,31 +55,6 @@ class StartMenu {
         }
     }
 
-    // async runLists(option) {
-    //     if (option === menuEnums.listMenu.AUTHORLIST) {
-    //         const authors = await this.database.getAllAuthors()
-    //         this.print.fromArray(authors)
-    //     } else if (option === menuEnums.listMenu.AUTHORTIMESREAD) {
-    //         const info = await this.database.getAuthorsAndTimesRead()
-    //         for (let i = 0; i < info.length; i++) {
-    //             this.print.authorList(info[i])
-    //         }
-    //     } else if (option === menuEnums.listMenu.BOOKTITLES) {
-    //         const titles = await this.database.getAllBookTitles()
-    //         this.print.fromArray(titles)
-    //     } else if (option === menuEnums.listMenu.REVIEWEDBOOKS) {
-    //         const books = await this.database.getReviewedBooks()
-    //         for (let i = 0; i < books.length;i++) {
-    //             this.print.reviewedBooks(books[i])
-    //         }
-    //     } else if (option === menuEnums.listMenu.MEMBERS) {
-    //         const usernames = await this.database.getAllUsernames()
-    //         this.print.fromArray(usernames)
-    //     } else if (option === menuEnums.listMenu.RETURN) {
-    //         this.run()
-    //     }
-    // }
-
     async runMemberMenu(answer, username) {
         if (answer === menuEnums.memberAndBookMenu.INFO) {
             const user = await this.database.getUserInfo(username)
