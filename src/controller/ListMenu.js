f205cf03883033ae131d9c2b894413d8e414217c
Pernilla Göth <pg222jx@student.lnu.se>
2021-01-11 12:37:19 +0100

    Continuein return to main after output

--- src/controller/ListMenu.js
+++ src/controller/ListMenu.js
@@ -1,4 +1,5 @@
 const menuEnums = require('../view/menuEnums')
+const startApp = require('./startApp')
 const Print = require('../view/Print')
 const Database = require('../model/Database')
 
@@ -13,23 +14,33 @@ class ListMenu {
     async runLists(option) {
         if (option === menuEnums.listMenu.AUTHORLIST) {
             const authors = await this.database.getAllAuthors()
+            this.print.clearConsole()
             this.print.fromArray(authors)
+            startApp.startUp()
         } else if (option === menuEnums.listMenu.AUTHORTIMESREAD) {
             const info = await this.database.getAuthorsAndTimesRead()
+            this.print.clearConsole()
             for (let i = 0; i < info.length; i++) {
                 this.print.authorList(info[i])
             }
+            startApp.startUp()
         } else if (option === menuEnums.listMenu.BOOKTITLES) {
             const titles = await this.database.getAllBookTitles()
+            this.print.clearConsole()
             this.print.fromArray(titles)
+            startApp.startUp()
         } else if (option === menuEnums.listMenu.REVIEWEDBOOKS) {
             const books = await this.database.getReviewedBooks()
+            this.print.clearConsole()
             for (let i = 0; i < books.length;i++) {
                 this.print.reviewedBooks(books[i])
             }
+            startApp.startUp()
         } else if (option === menuEnums.listMenu.MEMBERS) {
             const usernames = await this.database.getAllUsernames()
+            this.print.clearConsole()
             this.print.fromArray(usernames)
+            startApp.startUp()
         } else if (option === menuEnums.listMenu.RETURN) {
             startApp.startUp()
         }
--- src/controller/StatisticMenu.js
+++ src/controller/StatisticMenu.js
@@ -38,21 +38,29 @@ class StatisticMenu {
         if (option === menuEnums.memberStatMenu.MEMBERFEMALES) {
             gender = menuEnums.genderMenu.FEMALE
             result = await this.database.countMembers(gender)
+            this.print.clearConsole()
             this.print.singleValue(result)
+            startApp.startUp()
         } else if (option === menuEnums.memberStatMenu.MEMBERMALES) {
             gender = menuEnums.genderMenu.MALE
             result = await this.database.countMembers(gender)
+            this.print.clearConsole()
             this.print.singleValue(result)
+            startApp.startUp()
         } else if (option === menuEnums.memberStatMenu.AGETITLE) {
             const age = await this.memberMenu.getAgeInput()
             const title = await this.bookMenu.getTitleInput()
             result = await this.database.getAgeTitleStatistics(age, title)
+            this.print.clearConsole()
             this.print.singleValue(result)
+            startApp.startUp()
         } else if (option === menuEnums.memberStatMenu.GENDERYEAR) {
             const gender = await this.memberMenu.getGenderOptions()
             const year = await this.bookMenu.getYearInput()
             result = await this.database.getGenderYearStatistics(gender, year)
+            this.print.clearConsole()
             this.print.singleValue(result)
+            startApp.startUp()
         } else if (option === menuEnums.memberStatMenu.RETURN) {
             startApp.startUp()
         } 
@@ -65,18 +73,24 @@ class StatisticMenu {
         } else if (option === menuEnums.bookStatMenu.SEARCHAUTH) {
             const author = await this.bookMenu.getAuthorInput() 
             const titles = await this.database.getTitles(author)
+            this.print.clearConsole()
             for (let i = 0; i < titles.length; i++) {
                 this.print.singleValue(titles[i].title)
             }
+            startApp.startUp()
         } else if (option === menuEnums.bookStatMenu.SEARCHTITLE) {
             const title = await this.bookMenu.getInput()
             const info = await this.database.getStatistics(title)
+            this.print.clearConsole()
             this.print.statistics(info)
+            startApp.startUp()
         } else if (option === menuEnums.bookStatMenu.AUTHORTIMESREAD) {
             const info = await this.database.getAuthorsAndTimesRead()
+            this.print.clearConsole()
             for (let i = 0; i < info.length; i++) {
                 this.print.authorList(info[i])
             }
+            startApp.startUp()
         } else if (option === menuEnums.bookStatMenu.RETURN) {
             startApp.startUp()
         } 
@@ -85,14 +99,19 @@ class StatisticMenu {
     async runPopularOptions(option) {
         if (option === menuEnums.popularMenu.TIMESREAD) {
             const book = await this.database.getMostReadBook()
+            this.print.clearConsole()
             this.print.mostReadBook(book)
+            startApp.startUp()
         } else if (option === menuEnums.popularMenu.TOTALSCORE) {
             const book = await this.database.getHighestTotalScore()
+            this.print.clearConsole()
             this.print.highestTotalScore(book)
             startApp.startUp()
         } else if (option === menuEnums.popularMenu.AVGSCORE) {
             const book = await this.database.getPopularBookAvgScore()
+            this.print.clearConsole()
             this.print.highestAvgScore(book)
+            startApp.startUp()
         } else if (option === menuEnums.popularMenu.RETURN) {
             startApp.startUp()
         }
